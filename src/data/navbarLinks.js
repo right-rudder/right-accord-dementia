b51e1da574bb47a6cf7f97e34d9258653699db14
lucascr <lcrodrigues1995@gmail.com>
2025-01-29 19:12:43 -0300

    Update navbarLinks.js

--- src/data/navbarLinks.js
+++ src/data/navbarLinks.js
@@ -3,32 +3,32 @@ export const navbarLinks = [
     name: "Home",
     link: "/#top",
   },
-  {
-    name: "Our Services",
-    link: "/services",
-    submenu: [
-      {
-        name: "100 Hour Inspection",
-        link: "/services/100-hour-inspection",
-      },
-      {
-        name: "Annual Inspection",
-        link: "/services/annual-inspection",
-      },
-      /*       {
-        name: "Sheet Metal Work",
-        link: "/services/sheet-metal-work",
-      },
-      {
-        name: "Light Avionics",
-        link: "/services/light-avionics",
-      }, */
-    ],
-  },
-  {
-    name: "Blog",
-    link: "/blog#top",
-  },
+  // {
+  //   name: "Our Services",
+  //   link: "/services",
+  //   submenu: [
+  //     {
+  //       name: "100 Hour Inspection",
+  //       link: "/services/100-hour-inspection",
+  //     },
+  //     {
+  //       name: "Annual Inspection",
+  //       link: "/services/annual-inspection",
+  //     },
+  //     /*       {
+  //       name: "Sheet Metal Work",
+  //       link: "/services/sheet-metal-work",
+  //     },
+  //     {
+  //       name: "Light Avionics",
+  //       link: "/services/light-avionics",
+  //     }, */
+  //   ],
+  // },
+  // {
+  //   name: "Blog",
+  //   link: "/blog#top",
+  // },
   {
     name: "Contact Us",
     link: "/#contact-us",
